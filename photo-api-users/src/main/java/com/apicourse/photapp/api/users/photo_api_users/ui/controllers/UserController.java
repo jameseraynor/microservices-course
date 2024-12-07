b023cf439515daaccb07a078a3854ef6330ecd83
jameseraynor <jaramillojjavier@gmail.com>
2024-12-07 09:03:16 -0600

    error with h2 console

--- photo-api-users/src/main/java/com/apicourse/photapp/api/users/photo_api_users/ui/controllers/UserController.java
+++ photo-api-users/src/main/java/com/apicourse/photapp/api/users/photo_api_users/ui/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.apicourse.photapp.api.users.photo_api_users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + environment.getProperty("local.server.port");
     }
-
-    
 
 }
